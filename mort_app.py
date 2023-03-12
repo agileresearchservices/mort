7181bd857e7f1d0b4f122396cd2b67e88e85f278
Kevin M. Butler <kmbutler@agileresearchservices.com>
2023-03-12 15:11:12 -0400

    roll back

--- mort_app.py
+++ mort_app.py
@@ -38,82 +38,17 @@ if query:
     st.markdown('---')
     st.write("## Additional Resources:")
 
-    # unique_docs = {}
-    # for doc in docs:
-    #     title = str(doc.metadata.get('title'))
-    #     short_description = str(doc.metadata.get('short_description'))
-    #     child_url = str(doc.metadata.get('url_child'))
-    #     parent_url = str(doc.metadata.get('url_parent'))
-    #     timestamp = datetime.strptime(str(doc.metadata.get('timestamp')), "%Y-%m-%d %H:%M:%S").strftime("%H:%M:%S")
-    #     key = title + '||' + short_description
-    #     if key not in unique_docs:
-    #         unique_docs[key] = {'child_urls': [(child_url, timestamp)],
-    #                             'parent_url': parent_url}
-    #     else:
-    #         unique_docs[key]['child_urls'].append((child_url, timestamp))
-
-    # for title_desc, doc_info in unique_docs.items():
-    #     st.write('## ' + title_desc.split('||')[0])
-    #     st.write('**Short Description:** ' + title_desc.split('||')[1])
-    #     st.markdown(f"[Start from beginning]({doc_info['parent_url']})")
-    #     st.write('**Jump to Moments:** ')
-    #     child_urls = []
-    #     for child_url, timestamp in doc_info['child_urls']:
-    #         child_urls.append(f"[{timestamp}]({child_url})")
-    #     st.write(', '.join(child_urls))
-    # unique_docs = {}
-    # for doc in docs:
-    #     title = str(doc.metadata.get('title'))
-    #     short_description = str(doc.metadata.get('short_description'))
-    #     child_url = str(doc.metadata.get('url_child'))
-    #     parent_url = str(doc.metadata.get('url_parent'))
-    #     timestamp = datetime.strptime(str(doc.metadata.get('timestamp')), "%Y-%m-%d %H:%M:%S").strftime("%H:%M:%S")
-    #     key = title + '||' + short_description
-    #     if key not in unique_docs:
-    #         unique_docs[key] = {'child_urls': {child_url: {timestamp}},
-    #                             'parent_url': parent_url}
-    #     else:
-    #         if child_url not in unique_docs[key]['child_urls']:
-    #             unique_docs[key]['child_urls'][child_url] = {timestamp}
-    #         else:
-    #             unique_docs[key]['child_urls'][child_url].add(timestamp)
-
-    # for title_desc, doc_info in unique_docs.items():
-    #     st.write('## ' + title_desc.split('||')[0])
-    #     st.write('**Short Description:** ' + title_desc.split('||')[1])
-    #     st.markdown(f"[Start from beginning]({doc_info['parent_url']})")
-    #     st.write('**Jump to Moments:** ')
-    #     child_urls = []
-    #     for child_url, timestamps in doc_info['child_urls'].items():
-    #         sorted_timestamps = sorted(timestamps, key=lambda x: datetime.strptime(x, "%H:%M:%S"))
-    #         child_urls.append(f"[{', '.join(sorted_timestamps)}]({child_url})")
-    #     st.write(', '.join(child_urls))
-
     unique_docs = {}
     for doc in docs:
         title = str(doc.metadata.get('title'))
         short_description = str(doc.metadata.get('short_description'))
         child_url = str(doc.metadata.get('url_child'))
         parent_url = str(doc.metadata.get('url_parent'))
-        timestamp = datetime.strptime(str(doc.metadata.get('timestamp')), "%Y-%m-%d %H:%M:%S").time()
+        timestamp = datetime.strptime(str(doc.metadata.get('timestamp')), "%Y-%m-%d %H:%M:%S").strftime("%H:%M:%S")
         key = title + '||' + short_description
         if key not in unique_docs:
-            unique_docs[key] = {'child_urls': {child_url: {timestamp}},
+            unique_docs[key] = {'child_urls': [(child_url, timestamp)],
                                 'parent_url': parent_url}
         else:
-            if child_url not in unique_docs[key]['child_urls']:
-                unique_docs[key]['child_urls'][child_url] = {timestamp}
-            else:
-                unique_docs[key]['child_urls'][child_url].add(timestamp)
+            unique_docs[key]['child_urls'].append((child_url, timestamp))
 
-    for title_desc, doc_info in unique_docs.items():
-        st.write('## ' + title_desc.split('||')[0])
-        st.write('**Short Description:** ' + title_desc.split('||')[1])
-        st.markdown(f"[Start from beginning]({doc_info['parent_url']})")
-        st.write('**Jump to Moments:** ')
-        child_urls = []
-        for child_url, timestamps in doc_info['child_urls'].items():
-            sorted_timestamps = sorted(timestamps)
-            sorted_timestamp_strings = [ts.strftime("%H:%M:%S") for ts in sorted_timestamps]
-            child_urls.append(f"[{', '.join(sorted_timestamp_strings)}]({child_url})")
-        st.write(', '.join(child_urls))
